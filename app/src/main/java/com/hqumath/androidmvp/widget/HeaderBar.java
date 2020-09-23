ee9ecaf3dbd517f53993fce408c142cf0ab3c00c
gyd <guoyadi@live.cn>
2020-09-23 17:25:17 +0800

    1.自定义view HeaderBar

---
+++ app/src/main/java/com/hqumath/androidmvp/widget/HeaderBar.java
@@ -0,0 +1,19 @@
+package com.hqumath.androidmvp.widget;
+
+import android.content.Context;
+import android.widget.RelativeLayout;
+
+/**
+ * ****************************************************************
+ * 文件名称: HeaderBar
+ * 作    者: Created by gyd
+ * 创建时间: 2020/9/23 17:21
+ * 文件描述:
+ * 注意事项:
+ * ****************************************************************
+ */
+public class HeaderBar extends RelativeLayout {
+    public HeaderBar(Context context) {
+        super(context);
+    }
+}
