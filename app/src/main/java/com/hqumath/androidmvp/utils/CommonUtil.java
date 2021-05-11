56a8c3fc949effbc1043390b3e1d94764f2550e1
gyd <guoyadi@live.cn>
2021-05-11 15:38:57 +0800

    修改工具类

--- app/src/main/java/com/hqumath/androidmvp/utils/CommonUtil.java
+++ app/src/main/java/com/hqumath/androidmvp/utils/CommonUtil.java
@@ -65,29 +65,6 @@ public class CommonUtil {
         Toast.makeText(CommonUtil.getContext(), s, Toast.LENGTH_SHORT).show();
     }
 
-    //获取版本号
-    public static String getVersion() {
-        try {
-            PackageInfo pi = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
-            return pi.versionName;
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-            return "";
-        }
-    }
-
-    //获取imei
-    public static String getIMEI() {
-        String imei = "";
-        try {
-            TelephonyManager tm = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
-            imei = tm.getDeviceId();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return imei;
-    }
-
     public static void showKeyboard(Activity activity, EditText editText) {
         InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.showSoftInput(editText, 0);
@@ -101,26 +78,6 @@ public class CommonUtil {
         }
     }
 
-    public static String MapToString(HashMap<String, String> map) {
-        Iterator<Map.Entry<String, String>> i = map.entrySet().iterator();
-        if (!i.hasNext())
-            return "{}";
-
-        StringBuilder sb = new StringBuilder();
-        sb.append('{');
-        for (; ; ) {
-            Map.Entry<String, String> e = i.next();
-            String key = e.getKey();
-            String value = e.getValue();
-            sb.append(key);
-            sb.append(':');
-            sb.append(value);
-            if (!i.hasNext())
-                return sb.append('}').toString();
-            sb.append(',');//.append(' ');
-        }
-    }
-
     /**
      * 检查是否有网络
      */
@@ -130,77 +87,14 @@ public class CommonUtil {
         return info != null && info.isAvailable();
     }
 
-    //String 转 map
-    public static HashMap<String, String> String2HashMap(String data) {
-        int start = data.indexOf("{");
-        int end = data.indexOf("}");
-        String data0 = data.substring(start + 1, end);
-        HashMap<String, String> hashMap = new LinkedHashMap<>();
-        String[] data1 = data0.split(",");
-        for (String data2 : data1) {
-            String[] data3 = data2.split(":");
-            if (data3.length == 2) {
-                hashMap.put(data3[0].trim().replace("\"", ""), data3[1].trim().replace("\"", ""));
-            } else if (data3.length == 1) {
-                hashMap.put(data3[0].trim().replace("\"", ""), "");
-            }
-        }
-        return hashMap;
-    }
-
-    /**
-     * Base64字符串转换成图片
-     *
-     * @param string
-     * @return
-     */
-    public static Bitmap stringToBitmap(String string) {
-        Bitmap bitmap = null;
+    //获取软件版本号
+    public static String getVersion() {
         try {
-            byte[] bitmapArray = Base64.decode(string, Base64.DEFAULT);
-            bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.length);
-        } catch (Exception e) {
+            PackageInfo pi = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return pi.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
-        }
-        return bitmap;
-    }
-
-    /**
-     * 图片转换成base64字符串
-     *
-     * @param bitmap
-     * @return
-     */
-    public static String bitmapToString(Bitmap bitmap) {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] imgBytes = baos.toByteArray();// 转为byte数组
-        return Base64.encodeToString(imgBytes, Base64.DEFAULT);
-    }
-
-    // 计算字符串的MD5
-    public static String md5(String plainText) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(plainText.getBytes());
-            byte b[] = md.digest();
-            int i;
-            StringBuffer buf = new StringBuffer("");
-            for (int offset = 0; offset < b.length; offset++) {
-                i = b[offset];
-                if (i < 0)
-                    i += 256;
-                if (i < 16)
-                    buf.append("0");
-                buf.append(Integer.toHexString(i));
-            }
-            // 32位加密
-            return buf.toString();
-            // 16位的加密
-//             return buf.toString().substring(8, 24);
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-            return null;
+            return "";
         }
     }
 }
